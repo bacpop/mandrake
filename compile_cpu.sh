32b6de85eac3041e9d87b618880194c8c6be9dc5
John Lees <lees.john6@gmail.com>
2020-02-10 15:36:01 +0000

    Clean up compiler code
    Closes #3

--- compile_cpu.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++11 wtsne_cpu.cpp -o wtsne_cpu -lm -lgsl -lgslcblas -lgomp -fopenmp -Ofast -march=native -ffast-math
--- compile_gpu.sh
+++
@@ -1,2 +0,0 @@
-nvcc wtsne_gpu.cu -o  wtsne_gpu --cudart static -O3 --relocatable-device-code=false -gencode arch=compute_35,code=compute_35 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_50,code=sm_50 -lgsl -lgslcblas
-
