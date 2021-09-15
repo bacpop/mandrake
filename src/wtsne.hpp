7f18346db4e8279462a91e8d69a21c084e789419
John Lees <lees.john6@gmail.com>
2021-09-15 17:38:50 +0100

    Change formatting of clashes

--- src/wtsne.hpp
+++ src/wtsne.hpp
@@ -181,14 +181,17 @@ wtsne_init(const std::vector<uint64_t> &I, const std::vector<uint64_t> &J,
 
 template <typename real_t>
 inline void update_progress(const uint64_t iter, const uint64_t maxIter,
-                            const real_t eta, const real_t Eq, const unsigned long long int n_clashes) {
+                            const real_t eta, const real_t Eq,
+                            const unsigned long long int n_clashes) {
   if (iter % MAX(1, maxIter / 1000) == 0 || iter == maxIter - 1) {
     // Check for keyboard interrupt from python
     if (PyErr_CheckSignals() != 0) {
       throw py::error_already_set();
     }
-    fprintf(stderr, "%cOptimizing\t eta=%.4f Progress: %.1lf%%, Eq=%.10f clashes=%llu", 13,
-            eta, (real_t)iter / maxIter * 100, Eq, n_clashes);
+    fprintf(
+        stderr,
+        "%cOptimizing\t Progress: %.1lf%%, eta=%.4f, Eq=%.10f, clashes=%.2e",
+        13, eta, (real_t)iter / maxIter * 100, Eq, (real_t)n_clashes);
     fflush(stderr);
   }
 }
