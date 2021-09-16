6b640ee0c8ab7181fc490e9217a649209b2d6ff6
John Lees <lees.john6@gmail.com>
2021-09-16 17:37:13 +0100

    Add progress meter to pairsnp code

---
+++ src/progress.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <cstddef> // size_t
+#include <cstdint>
+#include <cstdio>
+
+inline uint64_t square_to_condensed(uint64_t i, uint64_t j, uint64_t n) {
+  return (n * i - ((i * (i + 1)) >> 1) + j - 1 - i);
+}
+
+class ProgressMeter {
+public:
+  ProgressMeter(size_t total, bool percent = false)
+      : total_(total), percent_(percent), count_(0) {
+    tick(0);
+  }
+
+  void tick_count(size_t count) {
+    count_ = count;
+    if (percent_) {
+      double progress = count_ / static_cast<double>(total_);
+      progress = progress > 1 ? 1 : progress;
+      fprintf(stderr, "%cProgress: %.1lf%%", 13, progress * 100);
+    } else {
+      size_t progress = count_ <= total_ ? count_ : total_;
+      fprintf(stderr, "%cProgress: %lu / %lu", 13, progress, total_);
+    }
+  }
+
+  void tick(size_t blocks) { tick_count(count_ + blocks); }
+
+  void finalise() {
+    if (percent_) {
+      fprintf(stderr, "%cProgress: 100.0%%\n", 13);
+    } else {
+      fprintf(stderr, "%cProgress: %lu / %lu\n", 13, total_, total_);
+    }
+  }
+
+private:
+  size_t total_;
+  bool percent_;
+  volatile size_t count_;
+};
