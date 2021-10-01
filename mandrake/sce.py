1445d3128782cd00dd64a5077d5b8fad3a180b76
John Lees <lees.john6@gmail.com>
2021-10-01 13:25:17 +0100

    Pass animate argument through to GPU from python

--- mandrake/sce.py
+++ mandrake/sce.py
@@ -69,6 +69,7 @@ def runSCE(I, J, dists, weight_file, names, SCE_opts):
                              nRepuSamp = SCE_opts['nRepuSamp'],
                              eta0 = SCE_opts['eta0'],
                              bInit = SCE_opts['bInit'],
+                             animated = SCE_opts['animate'],
                              cpu_threads = SCE_opts['cpus'],
                              device_id = SCE_opts['device_id'],
                              seed = SCE_opts['seed'])
