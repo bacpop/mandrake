203fdae5f85aba4dc16d17b555a9f6161aae84af
John Lees <lees.john6@gmail.com>
2021-10-05 10:19:11 +0100

    Reorder and speed up tests

--- test/run_test.py
+++ test/run_test.py
@@ -26,21 +26,21 @@ else:
 subprocess.run(python_cmd + " ../mandrake-runner.py --version", shell=True, check=True)
 
 sys.stderr.write("Running with each input type\n")
-subprocess.run(python_cmd + " ../mandrake-runner.py --alignment sub5k_hiv_refs_prrt_trim.fas --kNN 50 --cpus 2 --maxIter 10000000", shell=True, check=True)
-subprocess.run(python_cmd + " ../mandrake-runner.py --sketches listeria.h5 --threshold 0.02 --use-accessory --cpus 2 --maxIter 10000000", shell=True, check=True)
-subprocess.run(python_cmd + " ../mandrake-runner.py --accessory gene_presence_absence.Rtab --kNN 50 --cpus 2 --maxIter 10000000", shell=True, check=True)
-
-sys.stderr.write("Re-running with different SCE options\n")
-subprocess.run(python_cmd + " ../mandrake-runner.py --sketches listeria.h5 --kNN 50 --cpus 2 --maxIter 10000000", shell=True, check=True)
-subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 10000000 --eta0 2 --bInit 1 --perplexity 5", shell=True, check=True)
-subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 10000000 --animate --output animation", shell=True, check=True)
-subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 10000000 --labels data/labels.txt", shell=True, check=True)
-subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 10000000 --weight-file data/weights.txt", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --alignment sub5k_hiv_refs_prrt_trim.fas --kNN 50 --cpus 2 --maxIter 1000000", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --sketches listeria.h5 --threshold 0.02 --use-accessory --cpus 2 --maxIter 1000000", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --accessory gene_presence_absence.Rtab --kNN 50 --cpus 2 --maxIter 1000000", shell=True, check=True)
 
 # test updating order is correct
 sys.stderr.write("Processing can be turned off\n")
 # This won't necessarily work
-# subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 10000000 --no-preprocessing", shell=True, check=True)
-subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 10000000 --no-clustering", shell=True, check=True)
+# subprocess.run(python_cmd + " ../mandrake-runner.py --sketches listeria.h5 --kNN 50 --maxIter 10000000 --no-preprocessing", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --sketches listeria.h5 --kNN 50 --maxIter 1000000 --no-clustering", shell=True, check=True)
+
+sys.stderr.write("Re-running with different SCE options\n")
+subprocess.run(python_cmd + " ../mandrake-runner.py --sketches listeria.h5 --kNN 50 --cpus 2 --maxIter 1000000", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 1000000 --eta0 2 --bInit 1 --perplexity 5", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 1000000 --animate --output animation", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 1000000 --labels data/labels.txt", shell=True, check=True)
+subprocess.run(python_cmd + " ../mandrake-runner.py --distances mandrake.npz --maxIter 1000000 --weight-file data/weights.txt", shell=True, check=True)
 
 sys.stderr.write("Tests completed\n")
